3e5d1834b60714b5aceb36d7a7ead80b5285fdb0
seanaleid <seanaleid@gmail.com>
2020-01-29 12:27:39 -0500

    Add basic form and handleChange function
      *Adds a form, label, and input inside of NoteForm.js
      *Adds a handleChanges function and console logs the state

--- project/src/components/NoteForm.js
+++ project/src/components/NoteForm.js
@@ -2,8 +2,24 @@ import React, { useState } from "react";
 
 const NoteForm = props => {
   // console.log("this is our props",props);
+    const [note, setNote] = useState({
+        title: ''
+    });
+    
+    // onChange handler to control inputs
+    const handleChanges = e => {
+        setNote({title: e.target.value})
+        console.log(note);
+    }
 
-  return <form />;
+    // inside React JSX, we need to use htmlFor instead of for (for is a reserved word)
+    // can connect the input either by wrapping the input or by using the id on the input that matches the htmlFor
+    return (
+        <form>
+            <label htmlFor="title">Note Title</label>
+            <input id="title" type="text" onChange={handleChanges}/>
+        </form>
+    );
 };
 
 export default NoteForm;
